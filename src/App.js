321b77a8229454547cd44e726e2a08795ef01543
Z3alotik <74774101+Z3alotik@users.noreply.github.com>
2021-05-31 12:54:19 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React from "react";
+import './App.css';
+import {Container, Row, Col} from 'react-bootstrap';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+
+import NavigationBar from "./components/NavigationBar";
+import Welcome from "./components/Welcome";
+import Footer from "./components/Footer";
+import AddReservation from "./components/AddReservation";
+import ReservationList from "./components/ReservationList";
+
+function App() {
+    const marginTop = {
+       marginTop:"20px"
+    };
+  return (
+    <Router>
+        <NavigationBar/>
+        <Container>
+            <Row>
+                <Col lg={12} style={marginTop}>
+                    <Switch>
+                        <Route path="/" exact component={Welcome}/>
+                        <Route path="/add" exact component={AddReservation}/>
+                        <Route path="/list" exact component={ReservationList}/>
+                    </Switch>
+                </Col>
+            </Row>
+        </Container>
+        <Footer/>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/components/AddReservation.js
@@ -0,0 +1,176 @@
+import React, {Component} from "react";
+import {Card, Form, Button, Col} from 'react-bootstrap';
+import axios from "axios";
+import {DateTimePickerComponent} from '@syncfusion/ej2-react-calendars';
+import SuccessAlert from "./SuccessAlert";
+import ErrorAlert from "./ErrorAlert";
+
+export default class AddReservation extends Component{
+
+    constructor(props) {
+        super(props);
+        this.state = this.initialState;
+        this.reservationChange = this.reservationChange.bind(this);
+        this.submitReservation = this.submitReservation.bind(this);
+    }
+
+    initialState = {
+        name:'', surname:'', phone:'', car:'', date:''
+    }
+
+    resetReservation = () =>{
+        this.setState(() => this.initialState);
+    }
+/*
+    submitReservation = event => {
+        event.preventDefault();
+
+        const customer = {
+            name: this.state.name,
+            surname: this.state.surname,
+            phone: this.state.phone,
+            car: this.state.car,
+            date: this.state.date
+            };
+
+            axios.post("http://localhost:8080/rest/reservations", customer)
+                .then(response => {
+                    if(response.data != null) {
+                        this.setState(this.initialState);
+                        alert("Reservation saved Successfully");
+                        }
+                    });
+        }
+*/
+
+
+    submitReservation = event =>{
+        event.preventDefault()
+
+        const customer = {
+            name: this.state.name,
+            surname: this.state.surname,
+            phone: this.state.phone,
+            car: this.state.car,
+            date: this.state.date
+        };
+
+            axios.post('http://localhost:8080/rest/reservations',customer)
+                .then(response =>{console.log(response);
+                    this.setState({showAlert:true,success:true, message:"Successfull"})
+                    this.setState(this.initialState);})
+                .catch(error=> {console.log(error.response.status)
+                    if(error.response.status===400){this.setState({showAlert:true,success:false,message:"Error Ocured."})}
+                    if(error.response.status ===409){
+                        this.setState({showAlert:true,success:false, message:"No more then Two reservations at the same date and time"})
+                    }
+                    console.log(this.state.message)});
+
+        }
+
+    renderAlerts(){
+        if(this.state.showAlert){
+            if(this.state.success){
+                return <SuccessAlert/>
+            }
+            else if(this.state.validationError)
+            {
+                return <ErrorAlert message={this.state.validationError}/>
+            }
+            else{
+                return <ErrorAlert message={this.state.message}/>
+            }
+        }
+    }
+
+
+    reservationChange = event => {
+        this.setState({
+        [event.target.name]:event.target.value
+        });
+    }
+
+
+
+    render() {
+        const {name, surname, phone, car, date} = this.state;
+
+        return (
+            <div>
+                {this.renderAlerts()}
+                <Card className={"border border-dark bg-dark text-white"}>
+                    <Card.Header>Add Reservation</Card.Header>
+                    <Form onReset={this.resetReservation} onSubmit={this.submitReservation} id="reservationFormId">
+                        <Card.Body>
+                            <Form.Row>
+                                <Form.Group as={Col} controlId="formGridName">
+                                    <Form.Label>Name</Form.Label>
+                                    <Form.Control required autoComplete="off"
+                                                  type="text" name="name"
+                                                  value={name}
+                                                  onChange={this.reservationChange}
+                                                  className={"bg-dark text-white"}
+                                                  placeholder="Enter name" />
+                                </Form.Group>
+
+                                <Form.Group as={Col} controlId="formGridSurname">
+                                    <Form.Label>Surname</Form.Label>
+                                    <Form.Control required autoComplete="off"
+                                                  type="text" name="surname"
+                                                  value={surname}
+                                                  onChange={this.reservationChange}
+                                                  className={"bg-dark text-white"}
+                                                  placeholder="Enter surname" />
+                                </Form.Group>
+                            </Form.Row>
+
+                            <Form.Row>
+                                <Form.Group as={Col} controlId="formGridCar">
+                                    <Form.Label>Car</Form.Label>
+                                    <Form.Control required autoComplete="off"
+                                                  type="text" name="car"
+                                                  value={car}
+                                                  onChange={this.reservationChange}
+                                                  className={"bg-dark text-white"}
+                                                  placeholder="Enter Car" />
+                                </Form.Group>
+
+                                <Form.Group as={Col} controlId="formGridPhone">
+                                    <Form.Label>Phone Number</Form.Label>
+                                    <Form.Control required autoComplete="off"
+                                                  type="text" name="phone"
+                                                  value={phone}
+                                                  onChange={this.reservationChange}
+                                                  className={"bg-dark text-white"}
+                                                  placeholder="Enter Phone Number" />
+                                </Form.Group>
+                            </Form.Row>
+
+                            <Form.Row>
+                                <DateTimePickerComponent placeholder="Choose date"
+                                                         strictMode={true}
+                                                         format="yyyy-MM-dd HH:mm"
+                                                         step={60}
+                                                         type="date" name="date"
+                                                         value={date}
+                                                         onChange={this.reservationChange}
+                                >
+                                </DateTimePickerComponent>
+                            </Form.Row>
+                        </Card.Body>
+                        <Card.Footer style={{"textAlign":"right"}}>
+                            <Button size="sm" variant="success" type="submit">
+
+                                Create Reservation
+                            </Button>{' '}
+                            <Button size="sm" variant="info" type="reset">
+
+                                Reset
+                            </Button>
+                        </Card.Footer>
+                    </Form>
+                </Card>
+            </div>
+        );
+    }
+}
