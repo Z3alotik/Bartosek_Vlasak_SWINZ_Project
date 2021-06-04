e28a5da2276e1897ad5f9a7a46b1eee476d220c6
Z3alotik <74774101+Z3alotik@users.noreply.github.com>
2021-06-04 09:03:54 +0200

    Add files via upload

---
+++ src/components/SuccessAlert.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+class SuccessAlert extends React.Component{
+    render(){
+        return(
+            <div className="alert alert-success" role="alert">
+                <strong>SUCCESSFULLY ADDED</strong>
+            </div>
+        );
+    }
+
+}
+
+
+export default SuccessAlert
